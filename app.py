bf0df12fa0f04c2deeb726724cead90469aec664
varshavangasam <138878507+varshavangasam@users.noreply.github.com>
2024-04-18 15:12:19 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+import streamlit as st
+import requests
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=0f4c6f8afdc119f35ada8d579e57be79&language=en-US".format(movie_id)
+    response = requests.get(url, timeout=10)
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500" + poster_path
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]]['movie_id']  # corrected accessing movie_id column
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]]['title'])  # corrected accessing title column
+    return recommended_movie_names, recommended_movie_posters
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    st.columns(len(recommended_movie_names))
+    for name, poster in zip(recommended_movie_names, recommended_movie_posters):
+        st.text(name)
+        st.image(poster,width=200)
+
